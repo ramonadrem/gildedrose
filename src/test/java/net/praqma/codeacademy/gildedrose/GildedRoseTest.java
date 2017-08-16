1133ea083a17d3dc7801c7eb2c4a0d570b0191e9
ramonadrem <30963179+ramonadrem@users.noreply.github.com>
2017-08-16 15:49:17 +0200

    changing fixme into foo

--- src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
+++ src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
@@ -8,10 +8,10 @@ public class GildedRoseTest {
 
     @Test
     public void foo() {
-        Item[] items = new Item[] { new Item("fixme", 0, 0) };
+        Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
     }
 // blablabl
 }
